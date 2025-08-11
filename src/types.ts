b43f8704467004b8224928cef98f51e7005ee126
Guilherme Sa <guilsa001@gmail.com>
2025-08-10 23:49:22 -0300

    chore: Remove giscus

--- src/types.ts
+++ src/types.ts
@@ -8,9 +8,9 @@ export type SiteConfig = {
 		locale: string | string[] | undefined;
 		options: Intl.DateTimeFormatOptions;
 	};
-	comments: {
-		giscus: object;
-	};
+	// comments: {
+	// 	giscus: object;
+	// };
 };
 
 export type PaginationLink = {
